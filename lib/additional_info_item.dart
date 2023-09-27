78ea70d48fbba62eb8c11f488d1047fade8f5ded
codeforlife10 <lamaraman536536@gmail.com>
2023-09-27 18:46:58 +0545

    Weather app

---
+++ lib/additional_info_item.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class AdditionalInfoItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  const AdditionalInfoItem({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          size: 32,
+        ),
+        const SizedBox(height: 8),
+        Text(label),
+        const SizedBox(height: 8),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = 'f17dfd7f9d0f01e052c9cae777ab966e';
